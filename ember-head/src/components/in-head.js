4a08cc20d9ad36b8e030161f9f00356cfc22d06d
Edward Faulkner <edward@eaf4.com>
2022-07-08 21:41:50 -0400

    updates for octane-style

--- ember-head/src/components/in-head.js
+++ ember-head/src/components/in-head.js
@@ -1,10 +1,10 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { getOwner } from '@ember/application';
 import { getGlobalConfig } from '@embroider/macros';
 
 export default class extends Component {
   get head() {
-    let document = getDOM(this).documentElement;
+    let document = getOwner(this).lookup('service:-document').documentElement;
     let child = document.firstChild;
     while (child) {
       if (child.tagName === 'HEAD') {
@@ -16,24 +16,5 @@ export default class extends Component {
   }
   get inFastboot() {
     return getGlobalConfig().fastboot?.isRunning;
-  }
-}
-
-function getDOM(context) {
-  let { renderer } = context;
-  if (!renderer._dom) {
-    // pre glimmer2
-    let container = getOwner ? getOwner(context) : context.container;
-    let documentService = container.lookup('service:-document');
-    if (documentService) {
-      return documentService;
-    }
-    renderer = container.lookup('renderer:-dom');
-  }
-  if (renderer._dom && renderer._dom.document) {
-    // pre Ember 2.6
-    return renderer._dom.document;
-  } else {
-    throw new Error('ember-wormhole could not get DOM');
   }
 }
